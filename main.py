fc451868a0d48156667a71f41e8ee9d63c85be79
hyy230 <2638936581@qq.com>
2025-09-13 17:09:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ year_gain=0.5
 
 print("{:<5}{:<10}{:<10}".format("年","地球体重","月球体重"))
 for year in range(1,11):
-    earth_current=earth_weight+year*year_gain
-    moon_current=earth_current*moon_ratio
-    print("{:<5}{:<10.2f}{:<10.2f}".format(year,earth_current,moon_current))
+    earth_current=earth_weight+year*year_gain
+    moon_current=earth_current*moon_ratio
+    print("{:<5}{:<10.2f}{:<10.2f}".format(year,earth_current,moon_current))
